b3620879d717988e596362f3cfad386954a0d9e3
Jan <o1103018>
2023-03-22 14:16:48 +0800

    v.2（颜芷莹）

--- app/src/main/java/tw/edu/pu/o1103018/about/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1103018/about/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         txv = findViewById(R.id.txv)
         txv.setOnClickListener({
             if(txv.text == "你好tcyang"){
-                txv.text = "海青班不錯"
+                txv.text = "海青班很棒，數媒更是好的方向"
             }
             else{
                 txv.text = "你好tcyang"
